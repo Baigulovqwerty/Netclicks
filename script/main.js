09731a8fe98f353e9b2d81a6f50eb52d61e35b92
Baigulovqwerty <61236287+Baigulovqwerty@users.noreply.github.com>
2020-05-27 19:15:12 +0700

    Main.js 2.0

--- script/main.js
+++ script/main.js
@@ -1,7 +1,11 @@
 // menu
 
-const leftMenu = document.querySelector('.left-menu');
-const hamburger = document.querySelector('.hamburger');
+const leftMenu = document.querySelector('.left-menu'),
+    hamburger = document.querySelector('.hamburger'),
+    tvShowList = document.querySelector('.tv-shows__list'),
+    modal = document.querySelector('.modal');
+
+
 
 hamburger.addEventListener('click', () => {
     leftMenu.classList.toggle('openMenu');
@@ -9,7 +13,8 @@ hamburger.addEventListener('click', () => {
 });
 
 document.addEventListener('click', event => {
-    if (!event.target.closest('.left-menu')) {
+    const target = event.target;
+    if (!target.closest('.left-menu')) {
         leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
     }
@@ -24,3 +29,43 @@ leftMenu.addEventListener('click', event => {
         hamburger.classList.add('open');
     }
 });
+
+tvShowList.addEventListener('click', event => {
+
+    event.preventDefault();
+
+    console.log(event);
+    const target = event.target;
+    const card = target.closest('.tv-card');
+    if (card) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
+});
+
+modal.addEventListener('click', event => {
+    if (event.target.closest('.cross') ||
+        event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+    }
+});
+
+
+const changeImage = event => {
+    const card = event.target.closest('.tv-shows__item');
+    console.log(card);
+    if (card) {
+        const img = card.querySelector('.tv-card__img');
+        if (img.dataset.backdrop) {
+            [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
+        }
+    }
+};
+
+tvShowList.addEventListener('mouseover', changeImage);
+tvShowList.addEventListener('mouseout', changeImage);
+
+
+
+
